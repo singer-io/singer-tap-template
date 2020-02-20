f42233a66d1b796b4245615a48a25cab09743a80
Aaron Steers <18150651+aaronsteers@users.noreply.github.com>
2020-02-19 21:14:06 -0800

    improved bookmark handling

--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -64,6 +64,7 @@ def sync(config, state, catalog):
         # TODO: initialize key and bookmark columns
         key_columns = ["id"]
         bookmark_column = "id"
+        is_sorted = True  # whether data is sorted ascending on bookmark value
 
         singer.write_schema(
             stream_name=stream_id, schema=stream.schema, key_properties=key_columns,
@@ -72,13 +73,21 @@ def sync(config, state, catalog):
         # TODO: delete and replace this inline function with your own data retrieval process:
         tap_data = lambda: [{"id": x, "name": "row${x}"} for x in range(1000)]
 
+        max_bookmark = None
         for row in tap_data():
             # TODO: place type conversions or transformations here
 
             # write one or more rows to the stream:
-            singer.write_records(stream_id, [row]) 
+            singer.write_records(stream_id, [row])
             if bookmark_column:
-                singer.write_state({stream_id: row[bookmark_column]})
+                if is_sorted:
+                    # update bookmark to latest value
+                    singer.write_state({stream_id: row[bookmark_column]})
+                else:
+                    # if data unsorted, save max value until end of writes
+                    max_bookmark = max(max_bookmark, row[bookmark_column])
+        if bookmark_column and not is_sorted:
+            singer.write_state({stream_id: max_bookmark})
     return
 
 
